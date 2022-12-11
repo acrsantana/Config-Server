cd3cc6d79846fb9e8a5a3b54b7b78a78bd792065
Augusto Cezar Ribeiro Santana <augusto.santana@al.infnet.edu.br>
2022-12-10 22:15:24 -0300

    Initial Commit

---
+++ src/test/java/br/com/acrtech/planningpoker/config/ConfigServerApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.acrtech.planningpoker.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class ConfigServerApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
